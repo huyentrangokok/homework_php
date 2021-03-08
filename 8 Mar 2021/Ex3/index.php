dcfa7abc3e6ce54633e276f449d45c1be56e4791
Bui Trang <buithihuyentrangsp1@gmail.com>
2021-03-08 16:22:05 +0700

    update Ex3_8 Mar 2021 Signed-off-by: Bui Trang <buithihuyentrangsp1@gmail.com>

---
+++ 8 Mar 2021/Ex3/index.php
@@ -0,0 +1,9 @@
+<?php
+	$a = 10;
+	$b = 4;
+	echo "<pre>Tổng của hai số ".$a." và ".$b." là : ".$a+$b;
+	echo "<pre>Hiệu của hai số ".$a." và ".$b." là : ".$a-$b;
+	echo "<pre>Tích của hai số ".$a." và ".$b." là : ".$a*$b;
+	echo "<pre>Thương của hai số ".$a." và ".$b." là : ".$a/$b;
+	echo "<pre>Chia dư của hai số ".$a." và ".$b." là : ".$a%$b;
+?>
