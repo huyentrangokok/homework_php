3eff4c3acfdf83df50e4ac90356794cca7c0b533
Bui Trang <buithihuyentrangsp1@gmail.com>
2021-03-12 15:13:08 +0700

    update Ex5_Exercises_9 Mar 2021 Signed-off-by: Bui Trang <buithihuyentrangsp1@gmail.com>

--- 9 Mar 2021/Exercises/Ex5/index.php
+++ 9 Mar 2021/Exercises/Ex5/index.php
@@ -1,3 +1,12 @@
 <?php
-	echo 'Độ dài chuỗi là: '.strlen('rayy@example.com');
-?>
+    $str = "rayy@gmail.com";
+    $seachStr = "rayy";
+    $result = "";
+    for ($i = 0; $i < strlen($str); $i++) {
+        $result .= $str[$i];
+        if ($i == strlen($seachStr) - 1) {
+            break;
+        }
+    }
+    echo "Chuỗi cần lấy là: $result";
+    ?>
